19fe32d0ea92901821470996367f0e0ffe503be1
financiali <ali@freelancer.run>
2019-09-26 11:29:07 +0100

    - implement screen recording

--- nodes/adb-device/adb-device.js
+++ nodes/adb-device/adb-device.js
@@ -9,18 +9,12 @@ module.exports = function (RED) {
 
         var node = this;
         var timer;
-
-        var msg = {};
-
-        var _this = this;
-
-
         node.on("input", function (msg) {
-            _this.status({fill: "yellow", shape: "dot", text: 'ready'});
+            node.status({fill: "yellow", shape: "dot", text: 'ready'});
             client.listDevices()
                 .then(function (devices) {
                     msg.payload = devices;
-                    _this.status({fill: "green", shape: "dot", text: 'listed'});
+                    node.status({fill: "green", shape: "dot", text: 'listed'});
                     node.send([msg]);
                 })
                 .catch(function (err) {
@@ -40,7 +34,7 @@ module.exports = function (RED) {
         var timerStatus = function () {
             timer = setTimeout(function () {
                 clearTimeout(timer);
-                _this.status({});
+                node.status({});
             }, 1000);
         }
     }
@@ -50,21 +44,17 @@ module.exports = function (RED) {
         RED.nodes.createNode(this, config);
 
         var node = this;
-
-        var msg = {};
         var timer;
 
-        var _this = this;
-
         node.on("input", function (msg) {
-            _this.status({fill: "yellow", shape: "dot", text: 'getting'});
+            node.status({fill: "yellow", shape: "dot", text: 'getting'});
             var device_id = msg.device_id || msg.payload.device_id || config.device_id || null;
             if (!device_id)
                 return node.warn('device_id required', msg);
 
 
             client.getFeatures(device_id).then(function (features) {
-                _this.status({fill: "green", shape: "dot", text: 'getted'});
+                node.status({fill: "green", shape: "dot", text: 'getted'});
                 msg.payload = {
                     features: features
                 };
@@ -72,7 +62,7 @@ module.exports = function (RED) {
                 timerStatus()
 
             }).catch(function (err) {
-                _this.status({fill: "red", shape: "dot", text: 'getted'});
+                node.status({fill: "red", shape: "dot", text: 'getted'});
                 msg.payload = {
                     error: err.message
                 };
@@ -89,7 +79,7 @@ module.exports = function (RED) {
         var timerStatus = function () {
             timer = setTimeout(function () {
                 clearTimeout(timer);
-                _this.status({});
+                node.status({});
             }, 1000);
         }
     }
@@ -99,14 +89,10 @@ module.exports = function (RED) {
         RED.nodes.createNode(this, config);
 
         var node = this;
-
-        var msg = {};
         var timer;
 
-        var _this = this;
-
         node.on("input", function (msg) {
-            _this.status({fill: "yellow", shape: "dot", text: 'getting'});
+            node.status({fill: "yellow", shape: "dot", text: 'getting'});
             var device_id = msg.device_id || msg.payload.device_id || config.device_id || null;
             var command = msg.command || msg.payload.command || config.command || null;
 
@@ -118,17 +104,15 @@ module.exports = function (RED) {
 
 
             client.shell(device_id, command).then(function (features) {
-                _this.status({fill: "green", shape: "dot", text: 'getted'});
-                msg.payload = {
-                    features: features
-                };
+                node.status({fill: "green", shape: "dot", text: 'getted'});
+                msg.payload = features;
                 node.send([msg]);
                 timerStatus()
 
             }).catch(function (err) {
-                _this.status({fill: "red", shape: "dot", text: 'getted'});
+                node.status({fill: "red", shape: "dot", text: 'getted'});
                 msg.payload = {
-                    error: err.message
+                    error: err
                 };
                 node.error(err.message, msg);
                 node.send([null, msg]);
@@ -143,7 +127,57 @@ module.exports = function (RED) {
         var timerStatus = function () {
             timer = setTimeout(function () {
                 clearTimeout(timer);
-                _this.status({});
+                node.status({});
+            }, 1000);
+        }
+    }
+
+    function AdbScreenshotNode(config) {
+        RED.nodes.createNode(this, config);
+
+        var node = this;
+        var timer;
+
+        node.on("input", function (msg) {
+            node.status({fill: "yellow", shape: "dot", text: 'getting'});
+            var device_id = msg.device_id || msg.payload.device_id || config.device_id || null;
+            var dir = msg.screenshot_path || msg.payload.screenshot_path || config.screenshot_path || '/sdcard';
+            var image_name = msg.image_name ? msg.image_name + '.png' : 'SCREEN_' + Date.now() + '.png'; // screencap -p /sdcard/$name
+
+            var path = dir + '/' + image_name;
+
+            var command = 'screencap -p ' + path;
+
+            if (!device_id)
+                return node.error('device_id required', msg);
+
+
+            client.shell(device_id, command).then(function (features) {
+                node.status({fill: "green", shape: "dot", text: 'getted'});
+                msg.payload = features;
+
+                node.send([msg]);
+                timerStatus()
+
+            }).catch(function (err) {
+                node.status({fill: "red", shape: "dot", text: 'getted'});
+                msg.payload = {
+                    error: err
+                };
+                node.error(err.message, msg);
+                node.send([null, msg]);
+                timerStatus()
+            })
+        });
+
+        node.on("close", function (done) {
+            done()
+        });
+
+        var timerStatus = function () {
+            timer = setTimeout(function () {
+                clearTimeout(timer);
+                node.status({});
             }, 1000);
         }
     }
@@ -151,5 +185,6 @@ module.exports = function (RED) {
     RED.nodes.registerType("adb-list-devices", AdbListDevicesNode);
     RED.nodes.registerType("adb-device-features", AdbDeviceFeatutesNode);
     RED.nodes.registerType("adb-shell", AdbShellNode);
+    RED.nodes.registerType("adb-screenshot", AdbScreenshotNode);
 
 };
